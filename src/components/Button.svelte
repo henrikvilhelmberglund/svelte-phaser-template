3a1bd15b643c5ef689333e47f4684d1a63a60806
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-04-02 16:50:05 +0200

    feat: add template based on Vue template

---
+++ src/components/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { MouseEventHandler } from 'svelte/elements';
+
+	type ButtonProps = {
+		onclick: MouseEventHandler<HTMLButtonElement>;
+		disabled?: boolean;
+    children: Snippet;
+	};
+	let { onclick, children, disabled }: ButtonProps = $props();
+</script>
+
+<button
+	{onclick}
+	{disabled}
+	class="m-1 rounded-lg border border-white p-2 text-white hover:border-blue-200 active:bg-blue-900 disabled:cursor-not-allowed">
+	{#if children}
+		{@render children()}
+	{:else}
+		Missing button text
+	{/if}
+</button>
